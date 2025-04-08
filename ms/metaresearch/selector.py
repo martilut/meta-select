552a619dae6d94bd3f52d5ce7c3919624e3550f9
martilut <jeykolton@gmail.com>
2025-04-08 19:38:45 +0300

    Added cv to selector

--- ms/metaresearch/selector.py
+++ ms/metaresearch/selector.py
@@ -9,6 +9,12 @@ class Selector(ABC):
     def name(self) -> str:
         ...
 
+    def __init__(
+            self,
+            cv: bool = False,
+    ) -> None:
+        self.cv = cv
+
     def compute_classification(
             self,
             x_train: pd.DataFrame,
@@ -16,7 +22,7 @@ class Selector(ABC):
             x_test: pd.DataFrame | None = None,
             y_test: pd.DataFrame | None = None,
     ) -> pd.DataFrame:
-        return self.compute_generic(x_train, y_train, x_test, y_test)
+        return self.compute_generic(x_train, y_train, x_test, y_test, task="class")
 
     def compute_regression(
             self,
@@ -25,7 +31,7 @@ class Selector(ABC):
             x_test: pd.DataFrame | None = None,
             y_test: pd.DataFrame | None = None,
     ) -> pd.DataFrame:
-        return self.compute_generic(x_train, y_train, x_test, y_test)
+        return self.compute_generic(x_train, y_train, x_test, y_test, task="reg")
 
     @abstractmethod
     def compute_generic(
@@ -34,26 +40,58 @@ class Selector(ABC):
             y_train: pd.DataFrame,
             x_test: pd.DataFrame | None = None,
             y_test: pd.DataFrame | None = None,
+            task: str = "class",
     ) -> pd.DataFrame:
         ...
 
     def compute(
             self,
-            x_train: pd.DataFrame,
-            y_train: pd.DataFrame,
-            x_test: pd.DataFrame | None = None,
-            y_test: pd.DataFrame | None = None,
+            x: pd.DataFrame,
+            y: pd.DataFrame,
+            split: dict | None = None,
     ) -> pd.DataFrame:
-        if self.is_classif(y=y_train):
-            return self.compute_classification(x_train, y_train, x_test, y_test)
+        if split is not None and self.cv:
+            res_list = []
+            for i in split:
+                x_train = x.iloc[split[i]["train"], :]
+                y_train = y.iloc[split[i]["train"], :]
+                x_test = x.iloc[split[i]["test"], :]
+                y_test = y.iloc[split[i]["test"], :]
+
+                if self.is_classif(y=y):
+                    res = self.compute_classification(
+                        x_train=x_train,
+                        y_train=y_train,
+                        x_test=x_test,
+                        y_test=y_test
+                    )
+                else:
+                    res = self.compute_regression(
+                        x_train=x_train,
+                        y_train=y_train,
+                        x_test=x_test,
+                        y_test=y_test
+                    )
+                res_list.append(res)
+            res = pd.concat(res_list).mean(axis=0)
         else:
-            return self.compute_regression(x_train, y_train, x_test, y_test)
+            if self.is_classif(y=y):
+                res = self.compute_classification(
+                    x_train=x,
+                    y_train=y
+                )
+            else:
+                res = self.compute_regression(
+                    x_train=x,
+                    y_train=y
+                )
+        return res
 
     def select(
             self,
             res: pd.DataFrame,
             k_best: int | None = None,
-    ) -> tuple[pd.DataFrame, list[str]]:
+    ) -> pd.DataFrame:
         filtered_df = self.__select__(res=res)
         filtered_df.sort_values(
             by="value",
@@ -61,12 +99,13 @@ class Selector(ABC):
             inplace=True,
             key=abs
         )
-        selected_df = filtered_df.dropna(axis="rows")
+        selected_df = filtered_df.dropna(axis="rows", how="any")
         selected_features = selected_df.index.tolist()
         if k_best is not None and k_best < len(selected_features):
             selected_features = selected_features[:k_best]
-
-        return selected_df, selected_features
+        selected_df["selected"] = [False] * len(selected_df)
+        selected_df.loc[selected_features, "selected"] = True
+        return selected_df
 
     @abstractmethod
     def __select__(self, res: pd.DataFrame) -> pd.DataFrame:
@@ -74,22 +113,15 @@ class Selector(ABC):
 
     def compute_select(
             self,
-            x_train: pd.DataFrame,
-            y_train: pd.DataFrame,
-            x_test: pd.DataFrame | None = None,
-            y_test: pd.DataFrame | None = None,
+            x: pd.DataFrame,
+            y: pd.DataFrame,
+            split: dict | None = None,
             k_best: int | None = None,
     ) -> pd.DataFrame:
-        if x_test is None:
-            x_test = x_train
-        if y_test is None:
-            y_test = y_train
-
         res = self.compute(
-            x_train=x_train,
-            y_train=y_train,
-            x_test=x_test,
-            y_test=y_test,
+            x=x,
+            y=y,
+            split=split,
         )
 
         return self.select(res=res, k_best=k_best)
