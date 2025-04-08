3b3cac059bfebf96fb8e18320fe172b3d453d865
martilut <jeykolton@gmail.com>
2025-04-08 19:00:00 +0300

    Added RK-RFE selector

--- ms/metaresearch/selectors/model_wrapper.py
+++ ms/metaresearch/selectors/model_wrapper.py
@@ -52,3 +52,63 @@ class RFESelector(Selector, ModelWrapper):
             if rank > self.rank_threshold:
                 res.iloc[i, 0] = None
         return res
+
+
+class RK_RFE(Selector, ModelWrapper):
+    @property
+    def name(self) -> str:
+        return "rk_rfe"
+
+    def __init__(
+            self,
+            model: MetaModel,
+            cv: bool = False, 
+            ntree: int = 200
+    ):
+        super().__init__(cv=cv)
+        super().__init__(model=model)
+        self.ntree = ntree
+
+    def compute_generic(
+            self,
+            x_train: pd.DataFrame,
+            y_train: pd.DataFrame,
+            x_test: pd.DataFrame | None = None,
+            y_test: pd.DataFrame | None = None,
+            task: str = "class",
+    ) -> pd.DataFrame:
+        datam = pd.concat(
+            [y_train.reset_index(drop=True), x_train.reset_index(drop=True)], 
+            axis=1
+        )
+        y_matrix = datam.iloc[:, 0].values
+        remaining_features = list(datam.columns[1:])
+        mse = float('inf')
+        selected_predictors = None
+
+        while len(remaining_features) >= 3:
+            rf = RandomForestRegressor(n_estimators=self.ntree)
+            rf.fit(datam[remaining_features], y_matrix)
+
+            predictions = rf.predict(datam[remaining_features])
+            test_error = (y_matrix - predictions) ** 2
+            mean_squared_error_val = np.mean(test_error)
+
+            importances = rf.feature_importances_
+            importance_df = pd.DataFrame({'feature': remaining_features, 'importance': importances})
+            importance_df = importance_df.sort_values(by='importance')
+
+            if mean_squared_error_val <= mse:
+                mse = mean_squared_error_val
+                selected_predictors = importance_df['feature'].tolist()
+
+            elim_feature = importance_df.iloc[0]['feature']
+            remaining_features = [f for f in remaining_features if f != elim_feature]
+
+        result_df = pd.DataFrame(index=selected_predictors)
+        result_df["value"] = np.arange(len(selected_predictors), 0, -1)
+
+        return result_df
+
+    def __select__(self, res: pd.DataFrame) -> pd.DataFrame:
+        return res
